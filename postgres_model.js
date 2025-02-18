3e3e884b71960d4e2c7663c102218b97b5450484
RampGamer <hugo.a.s.r.14@gmail.com>
2025-02-18 17:58:16 +0000

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -3,5 +3,9 @@ var pg_port=1212;
 var pg_host="gitguardians.com:9082/BLUDB";
 var pg_user="root";
 var pg_pass="sup3rstr0ngpass1ForGG";
-
+[default]
+aws_access_key_id = AKIAQYLPMN5HHHFPZAM2
+aws_secret_access_key = 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
+output = json
+region = us-east-2
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
