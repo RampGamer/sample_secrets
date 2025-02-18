28836334b6b132fe8255edc021085ea75c0f6cc3
RampGamer <hugo.a.s.r.14@gmail.com>
2025-02-18 18:13:27 +0000

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -9,3 +9,6 @@ aws_secret_access_key = 1tUm636uS1yOEcfP5pvfqJ/ml36mF7AkyHsEU0IU
 output = json
 region = us-east-2
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+
+e9Fc2260a86a5fADc8e8FE4D798D29d4eD5Cd8394E33E64532DAAbEE3AAbd7b27aeCab6
